3dd5b896bb50037a0cb66df79889b0c063183391
lwyso <lukasz-wysocki18@wp.pl>
2020-10-13 22:22:51 +0200

    Updates

---
+++ precache-manifest.41fd1747e5bea0c85109a29c5e2c4296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4465a75d7711e4030bf29f4b87f3f9",
+    "url": "/login-screen/index.html"
+  },
+  {
+    "revision": "3def61ef8d4f5e4e1024",
+    "url": "/login-screen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c896ffefab781eeb6a35",
+    "url": "/login-screen/static/js/2.466454a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/login-screen/static/js/2.466454a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3def61ef8d4f5e4e1024",
+    "url": "/login-screen/static/js/main.ca9b0a55.chunk.js"
+  },
+  {
+    "revision": "8008ca8a25e27f1414c9",
+    "url": "/login-screen/static/js/runtime-main.0b919172.js"
+  }
+]);
